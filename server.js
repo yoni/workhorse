a08fcf695374342a55ff159122f4d280ce16accc
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2010-07-18 18:03:04 -0400

    Better comments. Logging environment. Title for homepage.

--- server.js
+++ server.js
@@ -2,21 +2,23 @@
 var puts = require('sys').puts
 var express = require('express')
 
+// settings
+var port = 3000
+
 // initialize the application
 var app = express.createServer()
 
-// homepage
-app.get('/', function(req, res){
-  res.send("here's a problem to chew on")
+// Routing:
+app.get('/', function(request, response){
+  response.send("<title>workhorse</title>here's a problem to chew on")
+})
+app.post('/solution', function(request,response){
+  response.send('got your solution. thanks for helping out')
 })
 
-// solution submitted
-app.post('/solution', function(req,res){
-  res.send('got your solution. thanks for helping out')
-})
-
-// settings
-var port = 3000
 // fire it up
 app.listen(port)
+
+// log some useful details. TODO: replace this with real logging
 puts('Workhorse running at http://<your_domain>:' + port)
+puts('Environment is set to ' + app.set('env'))
