f0f1491cbe9cf6ee480866534ff687fd8f5a27da
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2010-08-01 16:05:03 -0400

    tabs -> spaces

--- examples/simple.js
+++ examples/simple.js
@@ -1,21 +1,21 @@
 var workhorse = require('../workhorse');
 
 workhorse({
-	// use a static problem server, which always returns the same problem
-	problem: function(callback) {
-		callback({
-			id: '1',
-			description: 'Add two numbers and send back the result',
-			solver: 'adder',
-			args: {
-				a: 2,
-				b: 3
-			}
-		})
-	},
-	// use a static solution taker, which simply returns a message that the solution has been received
-	solution: function(solution, problem_id, callback) {
-		callback('Got your solution for problem ' + problem_id + '. thanks for helping out\n');
-	}
+  // use a static problem server, which always returns the same problem
+  problem: function(callback) {
+    callback({
+      id: '1',
+      description: 'Add two numbers and send back the result',
+      solver: 'adder',
+      args: {
+        a: 2,
+        b: 3
+      }
+    })
+  },
+  // use a static solution taker, which simply returns a message that the solution has been received
+  solution: function(solution, problem_id, callback) {
+    callback('Got your solution for problem ' + problem_id + '. thanks for helping out\n');
+  }
 }).listen(8000);
 
